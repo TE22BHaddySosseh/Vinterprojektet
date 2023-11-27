3ef96c59f20041613ef527fc6e391283c9ae2cd9
Haddy Sosseh <Haddy.Sosseh@elev.ga.ntig.se>
2023-11-27 09:30:23 +0100

    start of story

---
+++ Vinterprojektet/Program.cs
@@ -0,0 +1,10 @@
+﻿string name = Console.ReadLine().ToLower();
+
+Console.WriteLine("In a cold and desolate stone tower, you reside, alone and shivering. You have no idea who brought you here, for what purpose, or even if you'll ever get out. All you known is that you're a princess. Well, you assume so. Not many common folk proudly display a pure golden crown adorned with jewels atop their head. Not like you would know regardless. All you've ever known is the inside of this tower, inside this bedroom, and all you have to your name is.. actually, do you even have a name?");
+
+if (name == "")
+{
+    Console.WriteLine("I can't call you Princess nothing. If we want to continue this story, our glorious heroine needs a proper name! Try again.");
+}
+
+Console.WriteLine("Ah, yes. Pardon me, Princess" + name + ". Now, where was I? Right. All you have to your name is the long floor length dress you're currently wearing, and the aforementioned crown atop your head. You don't know why, but you sit in hope of a handsome hero to come and rescue you. It just feels like thats what you're supposed to do, though a nagging feeling in your head is telling you that no one is coming.");
